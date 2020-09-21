fbe7a954c3ef1792c2e52af45595f325b1bcba1e
pancake <pancakeswap1@protonmail.com>
2020-09-21 15:44:13 +0800

    feat: add npx test buidler

---
+++ buidler.config.js
@@ -0,0 +1,49 @@
+usePlugin("@nomiclabs/buidler-waffle");
+
+// This is a sample Buidler task. To learn how to create your own go to
+// https://buidler.dev/guides/create-task.html
+task("accounts", "Prints the list of accounts", async () => {
+  const accounts = await ethers.getSigners();
+
+  for (const account of accounts) {
+    console.log(await account.getAddress());
+  }
+});
+
+usePlugin("@nomiclabs/buidler-truffle5");
+
+// You have to export an object to set up your config
+// This object can have the following optional entries:
+// defaultNetwork, networks, solc, and paths.
+// Go to https://buidler.dev/config/ to learn more
+module.exports = {
+  // This is a sample solc configuration that specifies which version of solc to use
+  solc: {
+    version: "0.6.12",
+    optimizer: {
+      enabled: true,
+      runs: 200
+    }
+  },
+
+  networks: {
+    buidlerevm: {
+    },
+    development: {
+      url: "http://127.0.0.1:7545",
+      port: 7545,
+      network_id: "101"
+    },
+    test: {
+      url: "http://127.0.0.1:7545",
+      port: 7545,
+      network_id: "*"
+    },
+  },
+  paths: {
+    sources: "./contracts",
+    tests: "./test",
+    cache: "./cache",
+    artifacts: "./artifacts"
+  }
+};
---
+++ test/MasterChef.test.js
@@ -0,0 +1,54 @@
+const { expectRevert, time } = require('@openzeppelin/test-helpers');
+const CakeToken = artifacts.require('CakeToken');
+const SyrupBar = artifacts.require('SyrupBar');
+const MasterChef = artifacts.require('MasterChef');
+const MockBEP20 = artifacts.require('MockBEP20');
+
+contract('MasterChef', ([alice, bob, carol, dev, minter]) => {
+    beforeEach(async () => {
+        this.cake = await CakeToken.new({ from: alice });
+        this.syrup = await SyrupBar.new(this.cake.address, { from: alice });
+        this.lp1 = await MockBEP20.new('LPToken', 'LP1', '1000', { from: alice });
+        this.lp2 = await MockBEP20.new('LPToken', 'LP2', '1000', { from: alice });
+        this.lp3 = await MockBEP20.new('LPToken', 'LP3', '1000', { from: alice });
+        this.chef = await MasterChef.new(this.cake.address, this.syrup.address, dev, '1000', '300', '1000', { from: alice });
+        await this.cake.transferOwnership(this.chef.address, { from: alice });
+        await this.syrup.transferOwnership(this.chef.address, { from: alice });
+        await this.chef.add('1000', this.lp1.address, true);
+        await this.chef.add('1000', this.lp2.address, true);
+        await this.chef.add('1000', this.lp3.address, true);
+    });
+
+
+    it('deposit/withdraw, staking/unstaking', async () => {
+      await time.advanceBlockTo('280');
+      await this.lp1.approve(this.chef.address, '10', { from: alice });
+      await this.chef.deposit(1, '2', { from: alice });
+      await time.advanceBlockTo('310');
+      await this.chef.deposit(1, '0', { from: alice });
+      await this.chef.deposit(1, '4', { from: alice });
+      await this.chef.deposit(1, '0', { from: alice });
+      assert.equal((await this.lp1.balanceOf(alice)).toString(), '994');
+      await this.chef.withdraw(1, '4', { from: alice });
+      assert.equal((await this.lp1.balanceOf(alice)).toString(), '998');
+      assert.equal((await this.cake.balanceOf(alice)).toString(), '3499');
+    })
+
+    it('staking/unstaking', async () => {
+      await this.lp1.approve(this.chef.address, '10', { from: alice });
+      await this.chef.deposit(1, '2', { from: alice }); //0
+      await this.chef.withdraw(1, '2', { from: alice }); //1
+
+      await this.cake.approve(this.chef.address, '250', { from: alice });
+      await this.chef.enterStaking('240', { from: alice }); //3
+      assert.equal((await this.syrup.balanceOf(alice)).toString(), '240');
+      assert.equal((await this.cake.balanceOf(alice)).toString(), '10');
+      await this.chef.enterStaking('10', { from: alice }); //4
+      assert.equal((await this.syrup.balanceOf(alice)).toString(), '250');
+      assert.equal((await this.cake.balanceOf(alice)).toString(), '249');
+      await this.chef.leaveStaking(250);
+      assert.equal((await this.syrup.balanceOf(alice)).toString(), '0');
+      assert.equal((await this.cake.balanceOf(alice)).toString(), '749');
+
+    });
+});
